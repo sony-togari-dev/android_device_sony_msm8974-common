f63266694cc2cf219b1de5d285ac5a6769f56379
xkeita <keita_nya@live.jp>
2017-06-07 00:05:41 +0000

    msm8974: Don't build QCMediaPlayer
    Not supported anymore
    
    Change-Id: I46aec7c6cab9d405c314338e4244d534759fb632

--- msm8974.mk
+++ msm8974.mk
@@ -71,13 +71,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(COMMON_PATH)/media_profiles.xml:system/etc/media_profiles.xml
 
-# Media
-PRODUCT_PACKAGES += \
-    qcmediaplayer
-
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
@@ -93,7 +86,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libOmxVdecHevc \
     libc2dcolorconvert \
-    libdashplayer \
     libdivxdrmdecrypt \
     libmm-omxcore \
     libstagefrighthw
