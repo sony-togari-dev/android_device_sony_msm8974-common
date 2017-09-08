d431a2103df80b71a639d6a9ed50482115cf8c16
Max Weffers <rcstar6696@gmail.com>
2017-09-08 22:09:18 +0200

    msm8974: bluetooth: Add HIDL bluetooth HAL

--- hidl.mk
+++ hidl.mk
@@ -3,6 +3,10 @@ PRODUCT_PACKAGES += \
 	android.hardware.audio@2.0-impl \
 	android.hardware.audio.effect@2.0-impl
 	
+# Bluetooth
+PRODUCT_PACKAGES += \
+	android.hardware.bluetooth@1.0-impl
+	
 # Display
   PRODUCT_PACKAGES += \
  	android.hardware.graphics.allocator@2.0-impl \
