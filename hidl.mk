d03fb12631b44d52556198c947b101c041eaa099
Max Weffers <rcstar6696@gmail.com>
2017-09-08 22:00:33 +0200

    msm8974: audio: Add HIDL Audio HAL

--- hidl.mk
+++ hidl.mk
@@ -1,3 +1,10 @@
+# Audio
+PRODUCT_PACKAGES += \
+	android.hardware.audio@2.0-impl \
+	android.hardware.audio.effect@2.0-impl
+	
 # WiFi
 PRODUCT_PACKAGES += \
-   android.hardware.wifi@1.0-service
+	android.hardware.wifi@1.0-service
+   
+
