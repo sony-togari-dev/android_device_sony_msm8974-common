d6b48222ec4898211457fde640139574bf12f8c1
Max Weffers <rcstar6696@gmail.com>
2017-09-08 22:01:30 +0200

    msm8974: display: Add HIDL Display HAL

--- hidl.mk
+++ hidl.mk
@@ -3,8 +3,14 @@ PRODUCT_PACKAGES += \
 	android.hardware.audio@2.0-impl \
 	android.hardware.audio.effect@2.0-impl
 	
+# Display
+  PRODUCT_PACKAGES += \
+ 	android.hardware.graphics.allocator@2.0-impl \
+ 	android.hardware.graphics.mapper@2.0-impl \
+ 	android.hardware.graphics.composer@2.1-impl
+	
 # WiFi
 PRODUCT_PACKAGES += \
 	android.hardware.wifi@1.0-service
-   
+  
 
