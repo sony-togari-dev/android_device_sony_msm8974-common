e15f7ad754ad5a9e15ff84c29bbefefae3d82b0f
Max Weffers <rcstar6696@gmail.com>
2017-09-08 22:14:26 +0200

    msm8974: Add HIDL Thermal HAL

--- hidl.mk
+++ hidl.mk
@@ -41,6 +41,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
 	
+# Thermal
+PRODUCT_PACKAGES += \
+   android.hardware.thermal@1.0-impl
+
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service
