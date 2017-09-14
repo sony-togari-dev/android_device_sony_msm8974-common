a57a1c59379314d000d2e4983f9c6bae2903dee6
Ethan Chen <intervigil@gmail.com>
2017-09-14 01:01:28 -0700

    msm8974: Add seccomp policy
    Change-Id: I1d5017228a4a9318f8a77ef4036532d64718ecbb

--- msm8974.mk
+++ msm8974.mk
@@ -122,6 +122,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     keycheck
 
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
+#  Mobile Data
 PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
