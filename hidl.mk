711248d45fa0d69120bc2edd733ab3f91d567b55
Yifan Hong <elsk@google.com>
2017-09-22 15:16:18 -0700

    msm8974-common: Uprev health to 2.0
    Test: adb logcat -e health
    Test: lshal --debug
    
    Change-Id: I3b14f087d95578d034898600592cb6fb357a9958

--- hidl.mk
+++ hidl.mk
@@ -40,8 +40,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # Keymaster
 PRODUCT_PACKAGES += \
