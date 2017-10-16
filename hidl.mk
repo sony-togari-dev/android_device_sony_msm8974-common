11aa27c11be97cbfb6fc59d3384f7681e8d0f9bb
Max Weffers <rcstar6696@gmail.com>
2017-10-16 10:08:47 +0200

    msm8974:add DRM for binderized Hals
    Change-Id: I45736c337dce0332336cd2c4202c5eb687d209ec

--- hidl.mk
+++ hidl.mk
@@ -12,6 +12,10 @@ PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl \
 	android.hardware.camera.device@1.0-impl
 	
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl 
+    
 # Display
 PRODUCT_PACKAGES += \
  	android.hardware.graphics.allocator@2.0-impl \
