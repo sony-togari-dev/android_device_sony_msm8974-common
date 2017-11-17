839a2989f0dbbdb6f2495401c4fb131d9d431e47
Max Weffers <rcstar6696@gmail.com>
2017-11-17 16:09:01 +0100

    msm8974: remove include of thermanger test mk
    Change-Id: Id985fc6631bac8b887f00b43e655d7543572aa9d

--- Android.mk
+++ Android.mk
@@ -3,6 +3,5 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_VENDOR),sony)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
-    include hardware/sony/thermanager/dummy.mk
 endif
 endif
