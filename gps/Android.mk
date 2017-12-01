9b3203b28b864b79a53d3885e6e15f2e5f9c4ec6
Alexander Diewald <Diewi@diewald-net.com>
2017-12-01 13:15:06 +0100

    sony-8974: gps: Fix gps conf link creation.
    * In order to allow linking gps.conf to gps_debug.conf although
      the "gps_debug.conf" file already exists, force the linking.
      This can happen for suqsequent builds that do not clean the
      "/system" folder.
    * If a "real" gps_debug.conf file shall be used (already during
      build time), this module must be removed anyways, so no
      interferrence is expected.
    
    Change-Id: I604c213f7410ceb52e1ced0d991daae1a5c757d0
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- gps/Android.mk
+++ gps/Android.mk
@@ -6,6 +6,6 @@ gps_debug_conf := gps_debug.conf
 gps_conf := gps.conf
 
 $(gps_conf_dir)/$(gps_debug_conf):
-		ln -s $(gps_conf) $(TARGET_OUT_ETC)/$(gps_debug_conf)
+		ln -sf $(gps_conf) $(TARGET_OUT_ETC)/$(gps_debug_conf)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(gps_conf_dir)/$(gps_debug_conf)
