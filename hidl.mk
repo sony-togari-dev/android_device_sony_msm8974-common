ebf06d84ebd55fc3c240918c4d3e8a88ac466570
mcgi5sr2 <mcgi5sr2@gmail.com>
2017-12-14 15:53:10 +0000

    sony-8974: Explicitly define treble services.
    * Treble services are expected to be described in the
      manifest file acc. to VINTF documentiation. This
      avoids relying on the auto-detection mechnanism.
    * Avoids several warnings in logs emmited by the auto
      detection mechanism.
    
    Change-Id: I130025bb34e01cac242e067e7c294461ef1dc802
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- hidl.mk
+++ hidl.mk
@@ -5,31 +5,34 @@ PRODUCT_PACKAGES += \
 	
 # Bluetooth
 PRODUCT_PACKAGES += \
-	android.hardware.bluetooth@1.0-impl
+	android.hardware.bluetooth@1.0-impl \
+	android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl-legacy \
 	camera.device@1.0-impl-legacy
-	
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl 
-    
+
 # Display
 PRODUCT_PACKAGES += \
- 	android.hardware.graphics.allocator@2.0-impl \
- 	android.hardware.graphics.mapper@2.0-impl \
- 	android.hardware.graphics.composer@2.1-impl
- 	
+	android.hardware.graphics.allocator@2.0-impl \
+	android.hardware.graphics.allocator@2.0-service \
+	android.hardware.graphics.mapper@2.0-impl \
+	android.hardware.graphics.composer@2.1-impl \
+	android.hardware.graphics.composer@2.1-service
+
 # Dumpstate
 PRODUCT_PACKAGES += \
-   android.hardware.dumpstate@1.0-service.sony
- 	
+	android.hardware.dumpstate@1.0-service.sony
+
 # FM
 PRODUCT_PACKAGES += \
 	android.hardware.broadcastradio@1.0-impl
- 	
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
@@ -40,19 +43,23 @@ PRODUCT_PACKAGES += \
  	
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+	android.hardware.keymaster@3.0-impl \
+	android.hardware.keymaster@3.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
- 	android.hardware.light@2.0-impl   
- 	
+	android.hardware.light@2.0-impl \
+	android.hardware.light@2.0-service
+
 # Memtrack
 PRODUCT_PACKAGES += \
-	android.hardware.memtrack@1.0-impl
- 	
+	android.hardware.memtrack@1.0-impl \
+	android.hardware.memtrack@1.0-service
+
 # NFC
 PRODUCT_PACKAGES += \
 	android.hardware.nfc@1.0-impl
+	
 # Netd
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0
@@ -63,28 +70,31 @@ PRODUCT_PACKAGES += \
 	
 # Power
 PRODUCT_PACKAGES += \
-	android.hardware.power@1.0-impl
+	android.hardware.power@1.0-impl \
+	android.hardware.power@1.0-service
 
-# Rendering	
+# Rendering
 PRODUCT_PACKAGES += \
-	android.hardware.renderscript@1.0-impl 
-	
+	android.hardware.renderscript@1.0-impl
+
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl
-	
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
 # Thermal
 PRODUCT_PACKAGES += \
-   android.hardware.thermal@1.0-impl
+   android.hardware.thermal@1.0-impl \
+   android.hardware.thermal@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
 	android.hardware.usb@1.0-service
- 
+
 # Vibrator
 PRODUCT_PACKAGES += \
-	android.hardware.vibrator@1.0-impl 
- 
+	android.hardware.vibrator@1.0-impl
+
 # WiFi
 PRODUCT_PACKAGES += \
 	android.hardware.wifi@1.0-service
