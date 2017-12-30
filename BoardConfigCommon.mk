e552b78551ad4dace86bce3aa645d42b2c0057fb
Artem Borisov <dedsa2002@gmail.com>
2017-12-30 16:03:20 +0300

    msm8974-common: Enable boot and system server dex-preopt
    As stated in https://android.googlesource.com/platform/build/+/418258c
    this is required in O-MR1 since sepolicy now prevents system server from
    loading anything from /data making our system extremely slow and janky.
    
    Change-Id: I215ec7ebe95981ef0b953fe938e12b83675795ab

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,6 +72,16 @@ BLUE_LED_PATH := /sys/class/leds/led:rgb_blue/brightness
 # Font
 EXTENDED_FONT_FOOTPRINT := true
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
+
 # Graphics
 USE_OPENGL_RENDERER := true
 TARGET_USES_ION := true
