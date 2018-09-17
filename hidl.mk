97a092b4a6eceaac6bf64a452d1789d2cf8124f8
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    msm8974-common: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- hidl.mk
+++ hidl.mk
@@ -71,7 +71,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 # Rendering
 PRODUCT_PACKAGES += \
