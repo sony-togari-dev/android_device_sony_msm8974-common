c45918402b8e4dbc6ca65c9b1e08d567d66f3f97
Adrian DC <radian.dc@gmail.com>
2018-09-23 12:28:04 +0200

    msm8974-common: Run the LEDs introduction in a sub-process
     * Due to the inline execution of the LEDs waves during
        the keycheck detection, the volume inputs would rarely
        be ignored and the access to recovery not given on
        first attempt, requiring a second try
    
     * Use the newly introduced finish_keycheck function to run
        the LEDs animation in background and finish the fork later
    
     * This change also improves the user interaction with init
        by performing the vibration feedback upon press rather
        that at the end of the LEDs waves for recovery boot
    
    Squashed:
    init: Make the header importable more than once
    
     * Being a global variable, the pid_introduce_keycheck would
        be redefined upon second inclusion of the header,
        breaking the compilation in this situation
    
     * Turn the variable into a private attribute of the class
        to keep it accessible only from it and fix the issue
    
    Change-Id: I28cdb8381814b255e28045ab16991966f0826c65

--- include/init_sony/init_board_device.h
+++ include/init_sony/init_board_device.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,8 @@
 #ifndef __INIT_BOARD_DEVICE_H__
 #define __INIT_BOARD_DEVICE_H__
 
+#include <unistd.h>
+
 #include "init_board_common.h"
 #include "init_prototypes.h"
 
@@ -28,15 +30,61 @@
 // Constants: devices controls
 #define DEV_BLOCK_FOTA_NUM 16
 
+// Constants: init configurations
+#define KEYCHECK_TIMEOUT 3
+
 // Class: init_board_device
 class init_board_device : public init_board_common
 {
+private:
+    // Variables: init device specific
+    pid_t pid_introduce_keycheck;
+
 public:
     // Board: introduction for keycheck
     virtual void introduce_keycheck()
     {
-        // LED boot selection colors
-        led_color(255, 0, 255);
+        // Launch the animation in a second thread
+        pid_introduce_keycheck = fork();
+        if (pid_introduce_keycheck == 0)
+        {
+            // Short vibration
+            vibrate(50);
+
+            // LEDs boot selection animation
+            led_color(255, 0, 255);
+            msleep(100);
+            led_color(0, 0, 0);
+            msleep(100);
+            led_color(255, 0, 255);
+            msleep(100);
+            led_color(0, 0, 0);
+            msleep(100);
+            led_color(255, 0, 255);
+            msleep(100);
+            led_color(0, 0, 0);
+            msleep(100);
+            led_color(255, 0, 255);
+            msleep(100);
+            led_color(0, 0, 0);
+            msleep(100);
+            led_color(255, 0, 255);
+            msleep(200);
+            _exit(1);
+        }
+    }
+
+    // Board: finalization of keycheck
+    virtual void finish_keycheck(bool recoveryBoot)
+    {
+        // Short vibration
+        if (recoveryBoot)
+        {
+            vibrate(100);
+        }
+
+        // Wait for the animated keycheck to end or kill it
+        system_exec_kill(pid_introduce_keycheck, KEYCHECK_TIMEOUT);
     }
 
     // Board: introduction for Android
@@ -49,8 +97,18 @@ public:
     // Board: introduction for Recovery
     virtual void introduce_recovery()
     {
-        // LED Recovery colors
+        // LEDs Recovery animation
         led_color(255, 100, 0);
+        msleep(1500);
+        led_color(0, 0, 0);
+    }
+
+    // Board: finish init execution
+    virtual void finish_init()
+    {
+        // Power off LED and vibrator
+        led_color(0, 0, 0);
+        vibrate(0);
     }
 
     // Board: set led colors
@@ -59,6 +117,12 @@ public:
         write_int(LED_RED_PATH "brightness", r);
         write_int(LED_GREEN_PATH "brightness", g);
         write_int(LED_BLUE_PATH "brightness", b);
+    }
+
+    // Board: set hardware vibrator
+    void vibrate(uint8_t strength)
+    {
+        write_int("/sys/class/timed_output/vibrator/enable", strength);
     }
 };
 
