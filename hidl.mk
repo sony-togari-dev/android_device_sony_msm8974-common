e5fb4cc6ad83865bdf57970ad62e4a93a5e22962
115ek <e.blosz@hotmail.de>
2018-10-14 12:17:10 +0200

    msm8974-common: remove unnecessary PRODUCT_PACKAGES
    We don't need them if we use old 8974 GPS lib
    
    Change-Id: If009a852023c6c61476d50d42705a58e6649d69a

--- hidl.mk
+++ hidl.mk
@@ -35,9 +35,7 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0 \
-    android.hardware.gnss-V1.0-java
+    android.hardware.gnss@1.0-impl
 
 # Health
 PRODUCT_PACKAGES += \
