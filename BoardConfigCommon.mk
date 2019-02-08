56c9e478056a46848bcce957e3c02b30ccb88bf3
Anton Hansson <hansson@google.com>
2019-02-08 17:56:34 +0000

    msm8974-common: Use generic arch variant
    Follow blueline which has not demonstrated any regressions
    after switching to a generic arch variant.
    
    Bug: 120773446
    Test: make
    Change-Id: I9e23963fcfa5ccaa912f2930a632458c4d2a692d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,7 +44,7 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := krait
+TARGET_CPU_VARIANT := generic
 TARGET_CUSTOM_DTBTOOL := dtbToolLineage
 
 # Defines for HW subsystems
