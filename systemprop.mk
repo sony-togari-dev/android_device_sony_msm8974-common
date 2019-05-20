735cd901efdd61a356945bf6e365eb1b8715d5c8
bohu <bohu@google.com>
2019-05-20 15:22:48 -0700

    msm8974-common: Disable codec2
    BUG: 133178866
    Change-Id: Ie6698604357adfb7c36d6655ea8a42aecad6b219

--- systemprop.mk
+++ systemprop.mk
@@ -92,6 +92,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.ccodec=0 \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0
 
