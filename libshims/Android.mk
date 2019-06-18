aa02cfa293d9d28139794e0bf6784ee2188440ac
Nikhil Punathil <nikhilpe@gmail.com>
2019-06-17 21:32:49 -0700

    msm8974-common: move libshim_camera to platform
    shinano needs updates to the GraphicBuffer sources that breaks rhine compatibility.
    Move the shim to platform to account for this divergence.
    
    Change-Id: I2262128059fc787e1a2cba3dc604deb3d2c40be5
    Signed-off-by: Nikhil Punathil <nikhilpe@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -70,34 +70,3 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 
 include $(BUILD_SHARED_LIBRARY)
-
-# libshim_camera
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    ui/GraphicBuffer.cpp \
-    ui/GraphicBufferAllocator.cpp \
-    ui/GraphicBufferMapper.cpp \
-    utils/Looper.cpp \
-    utils/VectorImpl.cpp
-
-LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/include \
-    external/safe-iop/include \
-    system/core/libutils
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libhardware \
-    liblog \
-    libui \
-    libsync \
-    libutils
-
-LOCAL_MODULE := libshim_camera
-
-LOCAL_PROPRIETARY_MODULE := true
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-
-include $(BUILD_SHARED_LIBRARY)
