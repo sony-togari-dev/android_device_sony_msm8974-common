e935ba4c6c958ce3f0d90fcc4359018aa49eeb24
Alessandro Astone <ales.astone@gmail.com>
2019-09-13 11:10:57 +0200

    Revert "msm8974-common: move GNSS HIDL HAL to passthrough"
    This reverts commit 65a819b59d00fab2b352b5e8ebc5f88582d91fb7.
    
    Change-Id: I9f579451889022825e3f7942db21848d9a09a7c3

--- hidl.mk
+++ hidl.mk
@@ -37,7 +37,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 # Health
 PRODUCT_PACKAGES += \
