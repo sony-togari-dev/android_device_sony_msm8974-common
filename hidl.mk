f50e545762f90121520cf4f3354ea26c25780fd9
Han Wang <416810799@qq.com>
2019-10-06 06:54:15 +0200

    msm8974-common: Switch to 1.2 version of qcom power HAL
    Change-Id: Ic9abcf182117810563dccee8dace282edefbbd4d

--- hidl.mk
+++ hidl.mk
@@ -68,7 +68,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Rendering
 PRODUCT_PACKAGES += \
