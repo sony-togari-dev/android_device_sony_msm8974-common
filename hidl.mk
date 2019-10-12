2d9ca59f1f6a2ac24eb8807cd1e98b7002c550a9
dianlujitao <dianlujitao@lineageos.org>
2019-10-12 10:12:37 +0800

    msm8974-common: Enable AOSP bluetooth audio HAL v2
    Change-Id: I4d12752ae5fb2829d6612647c8740c84967f5434

--- hidl.mk
+++ hidl.mk
@@ -2,6 +2,8 @@
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@6.0-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
     android.hardware.audio.effect@6.0-impl
 
 # Bluetooth
