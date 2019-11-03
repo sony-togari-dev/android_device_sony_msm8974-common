364cf9caf68360bc7ccfe97937958ad5585e8bf0
Arian <arian.kulmer@web.de>
2019-11-03 21:07:35 +0100

    msm8974-common: init: Adapt for Android 10
    * Since Android 10 we have two stage mounting, that means that we have a
      ramdisk.img in the first stage and later on system is mounted as root.
    
    * Previously we have moved root/init to root/init.real and added a symlink
      as root/init which leads to /sbin/init_sony which has been in
      root/sbin/init_sony. init_sony will remove the symlink and move the
      original init (init.real) back to root/init and start it when init_sony
      is done.
    
    * The two stage mounting for a-only devices introduced in Android 10 has
      changed this behaviour. root/init is a symlink to /system/bin/init by
      default.
    
    * Additional there is the first stage init wich is placed in ramdisk/init
      which kinda replaces the previous root/init. So we move the ramdisk/init
      to ramdisk/init.real and create a symlink under ramdisk/init to
      /sbin/init_sony which is now placed in ramdisk/sbin/init_sony. init_sony
      will proceed like it did before Android 10: init_sony will remove the
      symlink and move the original init (init.real) back to ramdisk/init
      (not root/init) and start it when init_sony is done.
    
     => In the end init_sony and it's dependencies are being moved from
        TARGET_ROOT_OUT to TARGET_RAMDISK_OUT.
    
    * toybox_static is moved from recovery/root/sbin to recovery/root/system/bin.
    
    Change-Id: I875e418472179508be9024c219abe61d641bdfb3

--- init/Android.mk
+++ init/Android.mk
@@ -1,22 +1,25 @@
-ramdisk_init        := $(TARGET_RAMDISK_OUT)/init
-ramdisk_init_real   := $(TARGET_RAMDISK_OUT)/init.real
-ramdisk_init_sony   := $(TARGET_RAMDISK_OUT)/bin/init_sony
-ramdisk_keycheck    := $(TARGET_RAMDISK_OUT)/bin/keycheck
-ramdisk_toybox_init := $(TARGET_RAMDISK_OUT)/bin/toybox_init
+# Debug current init_sony settings
+$(info device-sony-common-init: init_sony for "$(TARGET_DEVICE)", platform "$(PRODUCT_PLATFORM)", with '$(BOARD_SONY_INIT_FLAGS)')
+
+# Init overrides for init_sony
+ramdisk_init      := $(TARGET_RAMDISK_OUT)/init
+ramdisk_init_real := $(TARGET_RAMDISK_OUT)/init.real
 
 # If /init is a file and not a symlink then rename it to /init.real
 # and make /init be a symlink to /bin/init_sony (which will execute
 # /init.real, if appropriate.
-$(ramdisk_init_real): \
-	$(ramdisk_init) \
-	$(ramdisk_init_sony) \
-	$(ramdisk_keycheck) \
-	$(ramdisk_toybox_init)
-	if [ ! -L $(ramdisk_init) ]; then \
+$(ramdisk_init_real): $(ramdisk_init) $(TARGET_RECOVERY_ROOT_OUT)/system/bin/toybox_static $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/utilities/init_sony
+	$(hide) if [ ! -d $(TARGET_RAMDISK_OUT)/bin ]; then \
+		mkdir -p $(TARGET_RAMDISK_OUT)/bin; \
+	fi
+	cp $(TARGET_RECOVERY_ROOT_OUT)/system/bin/toybox_static $(TARGET_RAMDISK_OUT)/bin/toybox_init
+	cp $(PRODUCT_OUT)/utilities/keycheck $(TARGET_RAMDISK_OUT)/bin/keycheck
+	cp $(PRODUCT_OUT)/utilities/init_sony $(TARGET_RAMDISK_OUT)/bin/init_sony
+	$(hide) if [ ! -L $(ramdisk_init) ]; then \
 	  mv $(ramdisk_init) $(ramdisk_init_real); \
 	  ln -s /bin/init_sony $(ramdisk_init); \
 	else \
-	  echo "init.real: $(ramdisk_init) is already a symlink"; \
+	  echo "/init $(ramdisk_init) is already a symlink"; \
 	fi
 
 ALL_DEFAULT_INSTALLED_MODULES += $(ramdisk_init_real)
