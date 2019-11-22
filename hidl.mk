fd0750047a63fed6849a6507a4f7a5471e97ab80
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    msm8974-common: Switch to Audio HAL V6
    Switch to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on taimen and walleye
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8

--- hidl.mk
+++ hidl.mk
@@ -1,8 +1,9 @@
 # Audio
 PRODUCT_PACKAGES += \
-	android.hardware.audio@2.0-impl \
-	android.hardware.audio.effect@2.0-impl
-	
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl
+
 # Bluetooth
 PRODUCT_PACKAGES += \
 	android.hardware.bluetooth@1.0-service
