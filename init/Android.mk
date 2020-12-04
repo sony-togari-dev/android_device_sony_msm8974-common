dbbf8915ac773e9f60e882c964cf812a8e353ceb
115ek <e.blosz@hotmail.de>
2020-12-04 21:12:46 +0100

    msm8974-common: Move files from /sbin to /bin
    * Android 11 globally removed /sbin
      https://github.com/LineageOS/android_system_core/commit/2f74a26a2dbe092d63f1e46e6471369d4a19adce#diff-ac3a2ef33fb4b89653911e274441a0a00da7c30371c1a36af05f22d054ea4cce
    
    Change-Id: I148cf3ee7e3bef809eb4285ad9b3d9de4189d8fb

--- init/Android.mk
+++ init/Android.mk
@@ -1,11 +1,11 @@
 ramdisk_init        := $(TARGET_RAMDISK_OUT)/init
 ramdisk_init_real   := $(TARGET_RAMDISK_OUT)/init.real
-ramdisk_init_sony   := $(TARGET_RAMDISK_OUT)/sbin/init_sony
-ramdisk_keycheck    := $(TARGET_RAMDISK_OUT)/sbin/keycheck
-ramdisk_toybox_init := $(TARGET_RAMDISK_OUT)/sbin/toybox_init
+ramdisk_init_sony   := $(TARGET_RAMDISK_OUT)/bin/init_sony
+ramdisk_keycheck    := $(TARGET_RAMDISK_OUT)/bin/keycheck
+ramdisk_toybox_init := $(TARGET_RAMDISK_OUT)/bin/toybox_init
 
 # If /init is a file and not a symlink then rename it to /init.real
-# and make /init be a symlink to /sbin/init_sony (which will execute
+# and make /init be a symlink to /bin/init_sony (which will execute
 # /init.real, if appropriate.
 $(ramdisk_init_real): \
 	$(ramdisk_init) \
@@ -14,7 +14,7 @@ $(ramdisk_init_real): \
 	$(ramdisk_toybox_init)
 	if [ ! -L $(ramdisk_init) ]; then \
 	  mv $(ramdisk_init) $(ramdisk_init_real); \
-	  ln -s /sbin/init_sony $(ramdisk_init); \
+	  ln -s /bin/init_sony $(ramdisk_init); \
 	else \
 	  echo "init.real: $(ramdisk_init) is already a symlink"; \
 	fi
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := toybox_init
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := sbin/toybox_init
-LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)/sbin
+LOCAL_SRC_FILES    := bin/toybox_init
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)/bin
 include $(BUILD_PREBUILT)
