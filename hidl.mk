f28f9aac8bdccf91a4f043e931404d354967d7de
Jakub Pawlowski <jpawlowski@google.com>
2021-04-13 11:00:12 +0200

    msm8974-common: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- hidl.mk
+++ hidl.mk
@@ -2,7 +2,7 @@
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@6.0-impl \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     android.hardware.audio.effect@6.0-impl
 
